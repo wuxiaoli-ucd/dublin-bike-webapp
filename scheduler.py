55564ddf5a14aeebd94fd2cb15e61fa81d0c754b
wuxiaoli-ucd <wuxiaoli.ie@gmail.com>
2026-04-11 14:35:22 +0100

    update scheduler to run in aws

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,20 @@
 import time
+import subprocess
+import sys
 from datetime import datetime
-from scraper.s06_availability_scraper import main
+
 
 while True:
     try:
         print(f"Running scraper at {datetime.now()}...")
-        main()
+
+        subprocess.run(
+            [sys.executable, "-m", "scraper.s06_availability_scraper"],
+            check=True
+        )
+
         print("Done. Sleeping for 5 minutes...\n")
+
     except Exception as e:
         print("Error:", e)
 
